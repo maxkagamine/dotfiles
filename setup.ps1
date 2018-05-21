c02962c1259004f056f795d2b95f6e21373bf807
Max Kagamine <max@kagami.ne>
2018-05-20 20:31:56 -0400

    Set home directory; added gitignore to whitelist files

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,15 @@
 $title = $host.UI.RawUI.WindowTitle = "Environment setup"
 $global:currentTask = -1
 $global:taskCount = (Select-String "Start-Task" -Path $PSCommandPath).Matches.Count - 2
+
 function Start-Task($task) {
   Write-Progress -Activity $title -Status $task -PercentComplete (++$global:currentTask / $global:taskCount * 100)
+}
+
+function Assert-Success {
+  if ($LastExitCode -ne 0) {
+    throw "Command exited with non-zero exit code."
+  }
 }
 
 try {
@@ -20,6 +27,9 @@ try {
     throw "Developer mode not enabled."
   }
 
+  wsl sudo true
+  Assert-Success
+
   # Set environment variables
 
   Start-Task "Setting environment variables"
@@ -31,14 +41,27 @@ try {
   Start-Task "Allowing sudo without password"
 
   wsl sudo bash -c "grep -q NOPASSWD /etc/sudoers || echo $'\n%sudo\tALL=(ALL) NOPASSWD:ALL' | EDITOR='tee -a' visudo > /dev/null"
+  Assert-Success
+
+  # Set home directory
+
+  Start-Task "Setting home directory"
+
+  $user = $(wsl whoami)
+  $homeWin = Join-Path -Path $PSScriptRoot -ChildPath "home"
+  $homeWsl = wsl wslpath $homeWin.Replace("\", "/")
+
+  wsl sudo awk -i inplace -v user=$user -v home=$homeWsl '{ \$1 ~ \"^\" user && \$6=home; print }' FS=: OFS=: /etc/passwd
+  Assert-Success
 
   # Whoop
 
   Write-Progress -Activity $title -Status "Done" -PercentComplete 100
-  Write-Host "Done."
+  Write-Host "Done"
 
 } catch {
   Write-Error $_.Exception
+  Write-Progress -Activity $title -Completed
 }
 
 Write-Host -NoNewLine "Press any key to close...";
