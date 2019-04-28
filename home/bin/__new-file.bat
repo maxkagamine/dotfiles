6709520e1d0a1bee5fe336729dff6ee9e41894f3
Max Kagamine <max@kgmn.jp>
2019-04-27 22:35:24 -0400

    Consolidated ShellNew command batch files

--- home/bin/__new-file.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: Usage: __new-file PATH FILENAME
-:: Opens a file called FILENAME for editing in the directory of PATH
-:: PATH may include a filename which will be ignored
-:: Intended for use from a ShellNew command
-
-start "" "C:\Program Files\Microsoft VS Code\Code.exe" "%~dp1\%2"
