691e97042f503775ad8832272ee6dd0cf837f160
Max Kagamine <max@kgmn.jp>
2021-12-05 07:20:18 -0800

    Tweaked container ls to allow piping to less

--- mods/docker/.config/bashrc.d/docker.sh
+++ mods/docker/.config/bashrc.d/docker.sh
@@ -9,22 +9,27 @@ dc() {
     docker container "$@"
   else
     # Inspired by https://github.com/politeauthority/docker-pretty-ps
-    docker container ls -a --no-trunc --format "{{.State}} {{.Names}}
-ID:        {{.ID}}
-Image:     {{.Image}}
-Status:    {{.Status}}
-Network:   {{.Networks}}
-Ports:     {{.Ports}}
-Mounts:    {{.Mounts}}
+    docker container ls -a --no-trunc --format "{{.State}} ● {{.Names}}
+{{.State}} ID:        {{.ID}}
+{{.State}} Image:     {{.Image}}
+{{.State}} Status:    {{.Status}}
+{{.State}} Network:   {{.Networks}}
+{{.State}} Ports:     {{.Ports}}
+{{.State}} Mounts:    {{.Mounts}}
 " | \
-    perl -p0e '
-      s/^running (.*)/\e[32m● \1\e[m/gm;
-      s/^exited (.*?\n\n)/\e[1;30m● \1\e[m/gsm;
-      s/^\w+: *\n//gsm;
-      s/, ?/\n           /g;
-      s/^(ID:\s+\w{12}).*/\1/gm;
+    perl -pe '
+      if (/^exited/) {
+        s/,\s*/,\e[1;30m/g; # Make sure lists stay colored when split
+      }
+      s/,\s*/\n           /g;            # Split lists of ports/mounts
+      s/^[\w\s]+:\s+\n//m;               # Remove empty lines
+      s/(?<=^running )●.*/\e[32m$&\e[m/; # Color name & dot green if running
+      s/(?<=^exited ).*/\e[1;30m$&\e[m/; # Fade details of stopped containers
+      s/^(running|exited) //;            # Remove statuses used for regex
+      s/ID:\s+\w{12}\K\w{52}//;          # Short ids, since --no-trunc was used
     ' | \
-    head -n -1
+    head -n -1 | \
+    less -FRX
   fi
 }
 
