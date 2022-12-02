04cd2ea1034a6fd63b653e86837b9fcfd643f181
Max Kagamine <max@kgmn.jp>
2022-12-01 19:31:02 -0800

    yt-dlp: Prefer m4a or ogg for audio (don't reencode lossy), mkv for video (even if not merging)
    Initially I had opus>ogg in here as well, but this causes an error when
    embedding a thumbnail:
    
      mutagen.oggvorbis.OggVorbisHeaderError: no appropriate stream found
    
    Turns out .ogg should *only* be Vorbis. The correct extension for Opus
    is .opus, which is in fact an Opus stream in an OGG container and not
    the raw audio stream like with .aac:
    
      https://wiki.xiph.org/MIME_Types_and_File_Extensions

--- mods/yt-dlp/.config/bashrc.d/yt-dlp.sh
+++ mods/yt-dlp/.config/bashrc.d/yt-dlp.sh
@@ -3,4 +3,4 @@
 alias yt='yt-dlp'
 
 # '!=economy' prevents downloading niconico's low quality, high traffic mode videos
-alias ytmp3='yt -xf "(bestaudio/best)[format_id!=economy]" --audio-format mp3 --audio-quality 0'
+alias ytmp3='yt -xf "(bestaudio/best)[format_id!=economy]"'
