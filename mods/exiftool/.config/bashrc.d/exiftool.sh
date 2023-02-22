54448001c195f15f79be9ba2abd428035ccc1fd7
Max Kagamine <max@kgmn.jp>
2023-02-21 19:33:39 -0800

    Exiftool alias to strip metadata except ICC profile etc.

--- mods/exiftool/.config/bashrc.d/exiftool.sh
+++ mods/exiftool/.config/bashrc.d/exiftool.sh
@@ -1,3 +1,6 @@
 # shellcheck shell=bash
 
 alias exiftool='exiftool -g -overwrite_original'
+
+# https://photo.stackexchange.com/a/69742
+alias exifstrip='exiftool -all= -TagsFromFile @ -ColorSpaceTags'
