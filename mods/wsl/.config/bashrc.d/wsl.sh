8e5f3625fac368f2e31844e46f65b1dcd7043661
Max Kagamine <max@kgmn.jp>
2023-04-15 19:00:21 -0700

    wsl: Use wslview to open things

--- mods/wsl/.config/bashrc.d/wsl.sh
+++ mods/wsl/.config/bashrc.d/wsl.sh
@@ -2,25 +2,26 @@
 
 CDPATH='.:~:~/Projects:/mnt/c/Users/max/Projects:/mnt/s:/mnt/c/Users/max'
 
-exp() { w "${1:-.}" | x explorer.exe || true; }
-hide() { w "$@" | x attrib.exe +h; }
-unhide() { w "$@" | x attrib.exe -h; }
-recycle() { w "$@" | x nircmdc.exe moverecyclebin; }
+alias open='wslview'
+alias e='exp'
+alias ee='exp;exit'
 
-w() {
-  local path
-  for path; do
-    printf '%s\n' "$(wslpath -w "$path")"
-  done
+exp() {
+  (( $# == 0 )) && set .
+  wslview "$@"
 }
+
+hide() { n "$@" | x wslpath -w | x attrib.exe +h; }
+unhide() { n "$@" | x wslpath -w | x attrib.exe -h; }
+recycle() { n "$@" | x wslpath -w | x nircmdc.exe moverecyclebin; }
 
 # Alt+V pastes Windows paths as WSL paths
 bind -x '"\ev": __paste_wslpath'
 __paste_wslpath() {
   local before="${READLINE_LINE:0:$READLINE_POINT}"
   local after="${READLINE_LINE:$READLINE_POINT}"
-  local insert; insert=$(printf '%q' "$(
-    wslpath "$(unclip | sed -E 's/^"|"$//g')")")
+  local insert
+  insert=$(printf '%q' "$(wslpath "$(unclip | sed -E 's/^"|"$//g')")")
   READLINE_LINE="${before}${insert}${after}"
   ((READLINE_POINT += ${#insert}))
 }
