9209e09d4a1c10d85a8277c71921e4e615feb205
Max Kagamine <max@kgmn.jp>
2023-05-01 18:37:53 -0700

    ffmpeg: Small function for batch converting to flac

--- mods/ffmpeg/.config/bashrc.d/ffmpeg.sh
+++ mods/ffmpeg/.config/bashrc.d/ffmpeg.sh
@@ -3,3 +3,10 @@
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffplay='ffplay -hide_banner'
 alias ffprobe='ffprobe -hide_banner'
+
+flac() {
+  local f
+  for f; do
+    ffmpeg -i "$f" -compression_level 12 "${f%.*}.flac" || return $?
+  done
+}
