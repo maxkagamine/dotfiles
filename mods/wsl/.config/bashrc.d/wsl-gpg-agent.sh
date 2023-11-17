f8ebae587b823f35bb3b0c4dd0124607d3792d27
Max Kagamine <max@kgmn.jp>
2023-11-17 16:10:58 +0900

    Fixed SSH_AUTH_SOCK not set on subsequent shells

--- mods/wsl/.config/bashrc.d/wsl-gpg-agent.sh
+++ mods/wsl/.config/bashrc.d/wsl-gpg-agent.sh
@@ -1,9 +1,10 @@
 # shellcheck shell=bash
 # https://github.com/BlackReloaded/wsl2-ssh-pageant
 
+export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+
 if ! pgrep -f wsl2-ssh-pageant > /dev/null; then
   # SSH
-  export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
   rm -f "$SSH_AUTH_SOCK"
   (setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"$HOME/.local/bin/wsl2-ssh-pageant.exe" >/dev/null 2>&1 &)
 
