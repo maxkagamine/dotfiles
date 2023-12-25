e25e4d173c46262ee0ae8fda6c8b48ee4f36d49f
Max Kagamine <max@kgmn.jp>
2023-12-25 20:31:27 +0900

    upscale: Attempt to undo esrgan's image brightening using histmatch

--- mods/misc-utils/.config/bashrc.d/misc-utils.sh
+++ mods/misc-utils/.config/bashrc.d/misc-utils.sh
@@ -1,3 +1,3 @@
 # shellcheck shell=bash
 
-complete -f -W '--all --waifu2x --cugan --esrgan --auto -s -n -f --dry-run --verbose' upscale
+complete -f -W '--all --waifu2x --cugan --esrgan --auto -s -n -f --histmatch --dry-run --verbose' upscale
