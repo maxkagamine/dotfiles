40d81164841f29038a1886c1e5fd3688336f30d9
Max Kagamine <max@kgmn.jp>
2024-09-18 23:57:23 +0900

    misc-utils: Added aliases for mkanimedir etc.

--- mods/misc-utils/.config/bashrc.d/misc-utils.sh
+++ mods/misc-utils/.config/bashrc.d/misc-utils.sh
@@ -1,7 +1,36 @@
 # shellcheck shell=bash
 
+alias mad='mkanimedir'
+alias mar='mkanimereadme'
+alias mmd='mkmoviedir'
 alias up='upscale'
 
-complete -f -W '--all --waifu2x --cugan --esrgan --auto -s -n -f --histmatch --dry-run --verbose' upscale up
+complete -f -W '
+  --all
+  --waifu2x
+  --cugan
+  --esrgan
+  --auto
+  -s
+  -n
+  -f
+  --histmatch
+  --dry-run
+  --verbose
+  -h --help
+' upscale up
 
-complete -d -W '-p --prefix -s --suffix -d --delimiter -n --dry-run' flatten
+complete -d -W '
+  -p --prefix
+  -s --suffix
+  -d --delimiter
+  -n --dry-run
+  -h --help
+' flatten
+
+complete -f -d -W '
+  -z
+  --si
+  --paths-from-stdin
+  -h --help
+' weigh
