df3749e96dd2bb5fb85eb70f5be8f2e0db345171
Max Kagamine <max@kgmn.jp>
2024-12-08 12:07:30 +0900

    fzf: First Ctrl+T shows contents of current dir; pressing again recurses

--- mods/fzf/.config/bashrc.d/fzf.sh
+++ mods/fzf/.config/bashrc.d/fzf.sh
@@ -2,8 +2,12 @@
 
 . /usr/share/fzf/key-bindings.bash
 
-export FZF_CTRL_T_OPTS='--ansi'
-export FZF_CTRL_T_COMMAND='fd --hidden --exclude .git --color always .'
+# First Ctrl+T shows contents of current dir; pressing again recurses
+export FZF_CTRL_T_OPTS="
+  --ansi
+  --bind 'ctrl-t:reload(fd --hidden --exclude .git --color always .)'
+"
+export FZF_CTRL_T_COMMAND='ls -vAN --color=always --group-directories-first'
 
 export FZF_ALT_C_OPTS=$FZF_CTRL_T_OPTS
 export FZF_ALT_C_COMMAND="$FZF_CTRL_T_COMMAND --type d"
