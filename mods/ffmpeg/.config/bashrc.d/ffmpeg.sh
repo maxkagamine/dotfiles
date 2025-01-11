a0827698c5b52138a9505d6b63e65109cb9ab9ad
Max Kagamine <max@kgmn.jp>
2025-01-11 16:14:00 +0900

    ffmpeg: Function to calculate vmaf

--- mods/ffmpeg/.config/bashrc.d/ffmpeg.sh
+++ mods/ffmpeg/.config/bashrc.d/ffmpeg.sh
@@ -10,3 +10,19 @@ flac() {
     ffmpeg -i "$f" -compression_level 12 "${f%.*}.flac" || return $?
   done
 }
+
+vmaf() {
+  if (( $# < 2 )); then
+    echo 'Usage: vmaf <distorted> <reference> [<stream mapping>]' >&2
+    echo "Mapping should be the format '[0:a][1:b]' where 'a' and 'b' are the" >&2
+    echo "stream indexes from the distorted and references inputs, respectively." >&2
+    return 1
+  fi
+  local distorted=$1 reference=$2 stream_mapping=$3 output
+  if ! output=$(ffmpeg -i "$distorted" -i "$reference" \
+         -filter_complex "${stream_mapping}libvmaf" -f null - 2>&1) ||
+     ! grep --color=never -Po '(?<=VMAF score: ).*' <<<"$output"; then
+    echo "$output" >&2
+    return 1
+  fi
+}
