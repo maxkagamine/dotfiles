b31bfcfda1ed1e39f40716a3fe4a9d1cefdd0ad0
Max Kagamine <max@kgmn.jp>
2025-12-05 23:11:51 +0900

    docker: Removed ab-av1 alias

--- mods/docker/.config/bashrc.d/docker.sh
+++ mods/docker/.config/bashrc.d/docker.sh
@@ -53,9 +53,6 @@ ds() { # docker status
 alias sqlarserver='dr -v .:/srv:ro -p 3939:80 -e TZ=Asia/Tokyo kagamine/sqlarserver'
 alias sqlarserverd='dr -d -v .:/srv:ro -p 3939:80 -e TZ=Asia/Tokyo --name=sqlarserver kagamine/sqlarserver'
 
-# https://github.com/alexheretic/ab-av1 (image must be built locally)
-alias ab-av1='dr -v .:/videos -v ab-av1:/root/.cache/ab-av1 ab-av1'
-
 # https://github.com/w3c/epubcheck (image must be built locally)
 alias epubcheck='dr -v .:/data:ro epubcheck'
 
